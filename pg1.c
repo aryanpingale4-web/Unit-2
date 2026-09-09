8391669699ec4752f97b71efcf7e651b2f2eafaa
Aryan Pingale <aryanpingale4@gmail.com>
2026-09-09 22:10:34 +0530

    Add pg1.c with basic arithmetic operations

---
+++ pg1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a=10;
+    printf("a=%d\n",a);
+    a+=5;
+    printf("a=%d\n",a);
+    a-=3;
+    printf("a=%d\n",a);
+    a*=2;
+    printf("a=%d\n",a);
+    a/=4;
+    printf("a=%d\n",a);
+    a%=5;
+    printf("a=%d\n",a);
+    return 0;
+}
